d40ec7f492d1f5cc987d8922278092d3d08ea251
YukiWatanabe <yutaroud@gmail.com>
2023-12-24 05:02:25 +0900

    username null false

--- db/migrate/20231223173742_create_users.rb
+++ db/migrate/20231223173742_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :nickname
+      t.string :nickname, null: false
       t.timestamps
     end
   end
